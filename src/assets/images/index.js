f8b43e5ff61254e90f420e55041b15659829d9f1
Beginneraboutlife116 <pythonforeye@gmail.com>
2024-08-29 22:22:50 +0800

    Update images for home page

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -3,5 +3,71 @@ import LogoLgPng from "./logos/logo-lg.png";
 
 import AccentSearchSvg from "./icons/accent-search.svg";
 import CloseSvg from "./icons/close.svg";
+import ArrowRightSvg from "./icons/arrow-right.svg";
+import ArrowLeftSvg from "./icons/arrow-left.svg";
+import ArrowRightWhiteSvg from "./icons/arrow-right-white.svg";
 
-export { LogoSmPng, LogoLgPng, AccentSearchSvg, CloseSvg };
+import BannerSmPng from "./banners/banner-sm.png";
+import BannerPng from "./banners/banner.png";
+
+import Theme1Png from "./themes/theme-1.png";
+import Theme1SmPng from "./themes/theme-1-sm.png";
+import Theme2Png from "./themes/theme-2.png";
+import Theme2SmPng from "./themes/theme-2-sm.png";
+import Theme3Png from "./themes/theme-3.png";
+import Theme3SmPng from "./themes/theme-3-sm.png";
+import Theme4Png from "./themes/theme-4.png";
+import Theme4SmPng from "./themes/theme-4-sm.png";
+import Theme5Png from "./themes/theme-5.png";
+import Theme5SmPng from "./themes/theme-5-sm.png";
+import Theme6Png from "./themes/theme-6.png";
+import Theme6SmPng from "./themes/theme-6-sm.png";
+import Theme7Png from "./themes/theme-7.png";
+import Theme7SmPng from "./themes/theme-7-sm.png";
+import Theme8Png from "./themes/theme-8.png";
+import Theme8SmPng from "./themes/theme-8-sm.png";
+
+import CardArticle1Png from "./cards/article-1.png";
+import CardArticle1SmPng from "./cards/article-1-sm.png";
+import CardArticle2Png from "./cards/article-2.png";
+import CardArticle2SmPng from "./cards/article-2-sm.png";
+
+export {
+  LogoSmPng,
+  LogoLgPng,
+
+  // icons
+  AccentSearchSvg,
+  CloseSvg,
+  ArrowRightSvg,
+  ArrowLeftSvg,
+  ArrowRightWhiteSvg,
+
+  // banners
+  BannerSmPng,
+  BannerPng,
+
+  // themes
+  Theme1Png,
+  Theme1SmPng,
+  Theme2Png,
+  Theme2SmPng,
+  Theme3Png,
+  Theme3SmPng,
+  Theme4Png,
+  Theme4SmPng,
+  Theme5Png,
+  Theme5SmPng,
+  Theme6Png,
+  Theme6SmPng,
+  Theme7Png,
+  Theme7SmPng,
+  Theme8Png,
+  Theme8SmPng,
+
+  // cards
+  CardArticle1Png,
+  CardArticle1SmPng,
+  CardArticle2Png,
+  CardArticle2SmPng,
+};
